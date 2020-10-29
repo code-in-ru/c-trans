e55635d0b0c4297781b98e01dbd9ee5f0b8d92bb
vadim <feofanov@yandexlyceum.ru>
2020-10-29 19:50:54 +0500

    fix route name

--- settings.py
+++ settings.py
@@ -7,13 +7,13 @@ routes_set = requests.get(f'http://maxi-karta.ru/{CITY}/transport/query/routes')
 
 routes_ids = []
 for route in routes_set['routes']:
-    routes_ids.append(route['route_id'])
+    routes_ids.append(route['description'])
 
-for route_id in routes_ids:
+for route in routes_ids:
     data_set = requests.get(f'http://maxi-karta.ru/miass/transport/query/stations?route_id={route_id}').json()
     for station in data_set['stations']:
         direction = station['direction']
         coords = (station['lat'], station['lon'])
         name = station['name']
         station_id = station['station_id']
-        routes.STATION_LIST.append(routes.Station(route_id, (), name, coords, direction))  # Заполняем список остановок
+        routes.STATION_LIST.append(routes.Station(route, (), name, coords, direction))  # Заполняем список остановок
